49eea9fcec83bce614a19b13318cb81a4ca6aa24
Smitbhatt007 <cristianoronny234@gmail.com>
2021-06-16 21:41:32 +0530

    This is my first commit

---
+++ Hackerrank/Functions _n_C.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int max_of_four(int a, int b, int c, int d){
+    if (a > b && a > c && a > d)
+    {
+        return a;
+    }else if( b > a && b > c && b > d)
+    {
+        return b;
+    }else if( c > a && c >b && c > d)
+    {
+        return c;
+    }
+    else
+    {
+        return d;
+    }
+    
+    
+}
+
+
+int main() {
+    int x[3];
+    
+    scanf("%d %d %d %d", &x[0], &x[1], &x[2], &x[3]);
+    int ans = max_of_four(x[0], x[1], x[2], x[3]);
+    printf("%d", ans);
+    
+    return 0;
+}
